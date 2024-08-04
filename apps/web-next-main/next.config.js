9079bc2a62d7e95ebb5381ae3d3ce8175049a07a
Star <371132562@qq.com>
2024-08-04 21:10:43 +0800

    feat(根布局): 基本完成

---
+++ apps/web-next-main/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+import mdx from '@next/mdx';
+
+const withMDX = mdx();
+
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
+};
+
+export default withMDX(nextConfig);
