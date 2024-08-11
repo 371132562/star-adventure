2faac4bb0d6a31de96344fff8210708d642bafee
Star <371132562@qq.com>
2024-08-11 16:24:19 +0800

    feat(国际化): 增加中文文本

--- apps/web-doc/.vitepress/config.ts
+++ apps/web-doc/.vitepress/config.ts
@@ -5,24 +5,50 @@ export default defineConfig({
   lang: 'zh-CN',
   title: 'Star Doc',
   description: 'Adventure',
+  cleanUrls: true,
+  srcDir: './docs',
+  markdown: {
+    lineNumbers: true,
+    image: {
+      lazyLoading: true
+    }
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     search: {
-      provider: 'local'
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            }
+          }
+        }
+      }
     },
 
     nav: [
-      { text: '前端', link: '/docs/frontend/javascript/typeConversion' },
-      { text: '计算机', link: '/docs/computerScience/twosComplement' },
-      { text: '数据结构', link: '/docs/dataStructures/arrayAndLinkedList' },
-      { text: '网络', link: '/docs/network/connectionManagement' }
+      { text: '前端', link: '/frontend/javascript/typeConversion' },
+      { text: '计算机', link: '/computerScience/twosComplement' },
+      { text: '数据结构', link: '/dataStructures/arrayAndLinkedList' },
+      { text: '网络', link: '/network/connectionManagement' }
     ],
 
     sidebar: {
-      '/docs/frontend': [
+      '/frontend': [
         {
           text: 'JS',
-          items: [{ text: '类型转换', link: '/docs/frontend/javascript/typeConversion' }],
+          items: [{ text: '类型转换', link: '/frontend/javascript/typeConversion' }],
+
           collapsed: true
         },
         {
@@ -31,24 +57,32 @@ export default defineConfig({
           collapsed: true
         }
       ],
-      '/docs/computerScience/': [
+      '/computerScience/': [
         {
           text: '计算机组成原理',
-          items: [{ text: '补码', link: '/docs/computerScience/twosComplement' }]
+          items: [{ text: '补码', link: '/computerScience/twosComplement' }]
         }
       ],
-      '/docs/dataStructures/': [
+      '/dataStructures/': [
         {
           text: '数据结构',
-          items: [{ text: '数组和链表', link: '/docs/dataStructures/arrayAndLinkedList' }]
+          items: [{ text: '数组和链表', link: '/dataStructures/arrayAndLinkedList' }]
         }
       ],
-      '/docs/network/': [
+      '/network/': [
         {
           text: '网络',
-          items: [{ text: '三次握手和四次挥手', link: '/docs/network/connectionManagement' }]
+          items: [{ text: '三次握手和四次挥手', link: '/network/connectionManagement' }]
         }
       ]
+    },
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+    outline: {
+      label: '页面导航'
     }
 
     // socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
